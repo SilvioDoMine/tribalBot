1008cb45a212c0b9b91a5c101df81eccc8c10aab
Arthur Henrique <arthurhenrique@live.com>
2019-03-28 02:10:40 -0300

    Fix logic and typo between buy/sell resources
    Hopefully this wont cause any futher mistakes

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 /* **************
     Configs
 **************** */
-var comprarQuando   = 200;  // A partir de quanto pode começar a comprar recursos? Se digitar 0, desabilita a compra.
-var venderQuando    = 1000;  // A partir de quanto pode começar a vender recursos? Se digitar 0, desabilita a venda.
+var venderQuando    = 200;  // A partir de quanto pode começar a vender recursos? Se digitar 0, desabilita a venda.
+var comprarQuando   = 900;  // A partir de quanto pode começar a comprar recursos? Se digitar 0, desabilita a compra.
 var maxTransacoes   = 5;    // Você vai definir nas configurações a quantidade de transação máxima, por compra ou venda.
 var tempoDeReacao   = 10000;// Tempo de reação para cada update em mili-segundos.
 var modoDebug       = true; // Deseja ativar o modo de debug, com mais detalhes? True ou False;
@@ -97,6 +97,16 @@ function comprarMadeira() {
     var quantidadeCompra = 0;
     var vezesCompra = 0;
     
+    /* ========================================================
+    /   Verificação de Pontos Premium
+    /  ======================================================== */
+    // SÓ POR SEGURANÇA: Se por acaso, a quantidade de pontos premium
+    // for zero (o que nunca deve acontecer) já cancela a compra daqui mesmo.
+    if (ppDaAldeia == 0) {
+        consoleDebug('Você não tem pontos premium.');
+        return false;
+    }
+
     // Vamos comprar a quantidade de recursos igual a quantidade de pontos premium
     // Pois o usuário pode ter 1 ou 2 pontos premium. Porém há um problema, e se o 
     // Usuário tiver 100 pontos prêmium, vamos comprar 100? Não. É por isso que 
@@ -120,7 +130,6 @@ function comprarMadeira() {
     /* ========================================================
     /   Verificação do Comerciante
     /  ======================================================== */
-    
     // Agora vamos calcular se temos comerciantes suficientes!
     // Se a quantidade de comerciantes atuais multiplicado por 1000
     // (pois cada comerciante carrega no máximo 1000 recursos)
@@ -173,7 +182,7 @@ function comprarMadeira() {
     // Se a quantidade de madeira que você tem na aldeia, mais a quantidade de madeira que você
     // vai receber é maior que a quantidade disponível do seu armazem, então não podemos comprar.
     if ((madeiraDaAldeia + quantidadeCompra) > armazemDaAldeia) {
-        console.log('Infelizmente você está sem espaço no Armazém para comprar tudo, ou está sem comerciantes.');
+        console.log('Infelizmente você está sem espaço no Armazém para comprar tudo.');
         console.log('Vamos calcular quanto recurso você pode comprar com o espaço restante.');
      // Se o espaço for menor, então continuar a transação normalmente.
      } else {
@@ -191,82 +200,84 @@ function logicaGeral() {
     if (comerciantesAtualValue > 0) {
 
         // Se o preço venda definido pelo usuário for maior que 0;
-        // E se o preço do recurso for maior ou igual ao definido pelo usuário;
-        if (venderQuando > 0 && madeiraValue >= venderQuando) {
+        // E se o preço do recurso for menor ou igual ao definido pelo usuário;
+        if (venderQuando > 0 && madeiraValue <= venderQuando) {
             // E se a quantidade de recurso que eu tenho na aldeia for maior que o preço do recurso.
             if (madeiraDaAldeia >= madeiraValue) {
                 // Vende os recursos!
-                consoleDebug('O mercado de madeira está em alta! Venda madeira por ' + madeiraValue + '.');
+                consoleDebug('O mercado de madeira está em baixa! Venda madeira por ' + madeiraValue + '.');
             } else {
                 // Não há recursos suficientes para realizar a compra!
-                consoleDebug('O mercado de madeira está em alta! Mas você não possui recursos o suficiente.');
+                consoleDebug('O mercado de madeira está em baixa! Mas você não possui recursos o suficiente.');
             }
         }
 
         // Se o preço de compra definido pelo usuário não for maior que 0;
-        // Se o preço do recurso for menor ou igual ao valor que eu desejo comprar;
-        if (comprarQuando > 0 && madeiraValue <= comprarQuando) {
+        // Se o preço do recurso for maior ou igual ao valor que eu desejo comprar;
+        if (comprarQuando > 0 && madeiraValue >= comprarQuando) {
             // E se a quantidade de pontos prêmium que eu tenho na aldeia for maior que um;
             if (ppDaAldeia >= 1) {
                 // Compra os recursos agora!
-                consoleDebug('O mercado de madeira está em baixa! Compre madeira por ' + madeiraValue + '.');
+                consoleDebug('O mercado de madeira está em alta! Compre madeira por ' + madeiraValue + '.');
                 comprarMadeira();
             } else {
                 // Não há pontos premium suficientes para comprar os recursos agora!
-                consoleDebug('O mercado de madeira está em baixa! Porém você não possui madeira suficiente.');
+                consoleDebug('O mercado de madeira está em alta! Porém você não possui pontos premium suficiente.');
             }
         }
 
         // Se o preço venda definido pelo usuário for maior que 0;
-        // E se o preço do recurso for maior ou igual ao definido pelo usuário;
-        if (venderQuando > 0 && argilaValue >= venderQuando) {
+        // E se o preço do recurso for menor ou igual ao definido pelo usuário;
+        if (venderQuando > 0 && argilaValue <= venderQuando) {
             // E se a quantidade de recurso que eu tenho na aldeia for maior que o preço do recurso.
-            if (madeiraDaAldeia >= argilaValue) {
+            if (argilaDaAldeia >= argilaValue) {
                 // Vende os recursos!
-                consoleDebug('O mercado de argila está em alta! Venda argila por ' + argilaValue + '.');
+                consoleDebug('O mercado de argila está em baixa! Venda argila por ' + argilaValue + '.');
             } else {
                 // Não há recursos suficientes para realizar a compra!
-                consoleDebug('O mercado de argila está em alta! Mas você não possui recursos o suficiente.');
+                consoleDebug('O mercado de argila está em baixa! Mas você não possui recursos o suficiente.');
             }
         }
 
         // Se o preço de compra definido pelo usuário não for maior que 0;
-        // Se o preço do recurso for menor ou igual ao valor que eu desejo comprar;
-        if (comprarQuando > 0 && argilaValue <= comprarQuando) {
+        // Se o preço do recurso for maior ou igual ao valor que eu desejo comprar;
+        if (comprarQuando > 0 && argilaValue >= comprarQuando) {
             // E se a quantidade de pontos prêmium que eu tenho na aldeia for maior que um;
             if (ppDaAldeia >= 1) {
                 // Compra os recursos agora!
-                consoleDebug('O mercado de argila está em baixa! Compre argila por ' + argilaValue + '.');
+                consoleDebug('O mercado de argila está em alta! Compre argila por ' + argilaValue + '.');
+                comprarargila();
             } else {
                 // Não há pontos premium suficientes para comprar os recursos agora!
-                consoleDebug('O mercado de argila está em baixa! Porém você não possui argila suficiente.');
+                consoleDebug('O mercado de argila está em alta! Porém você não possui pontos.');
             }
         }
 
 
         // Se o preço venda definido pelo usuário for maior que 0;
-        // E se o preço do recurso for maior ou igual ao definido pelo usuário;
-        if (venderQuando > 0 && ferroValue >= venderQuando) {
+        // E se o preço do recurso for menor ou igual ao definido pelo usuário;
+        if (venderQuando > 0 && ferroValue <= venderQuando) {
             // E se a quantidade de recurso que eu tenho na aldeia for maior que o preço do recurso.
-            if (madeiraDaAldeia >= ferroValue) {
+            if (ferroDaAldeia >= ferroValue) {
                 // Vende os recursos!
-                consoleDebug('O mercado de ferro está em alta! Venda ferro por ' + ferroValue + '.');
+                consoleDebug('O mercado de ferro está em baixa! Venda ferro por ' + ferroValue + '.');
             } else {
                 // Não há recursos suficientes para realizar a compra!
-                consoleDebug('O mercado de ferro está em alta! Mas você não possui recursos o suficiente.');
+                consoleDebug('O mercado de ferro está em baixa! Mas você não possui recursos o suficiente.');
             }
         }
 
         // Se o preço de compra definido pelo usuário não for maior que 0;
-        // Se o preço do recurso for menor ou igual ao valor que eu desejo comprar;
-        if (comprarQuando > 0 && ferroValue <= comprarQuando) {
+        // Se o preço do recurso for maior ou igual ao valor que eu desejo comprar;
+        if (comprarQuando > 0 && ferroValue >= comprarQuando) {
             // E se a quantidade de pontos prêmium que eu tenho na aldeia for maior que um;
             if (ppDaAldeia >= 1) {
                 // Compra os recursos agora!
-                consoleDebug('O mercado de ferro está em baixa! Compre madeira por ' + ferroValue + '.');
+                consoleDebug('O mercado de ferro está em alta! Compre ferro por ' + ferroValue + '.');
+                comprarferro();
             } else {
                 // Não há pontos premium suficientes para comprar os recursos agora!
-                consoleDebug('O mercado de ferro está em baixa! Porém você não possui ferro suficiente.');
+                consoleDebug('O mercado de ferro está em alta! Porém você não possui pontos.');
             }
         }
     
